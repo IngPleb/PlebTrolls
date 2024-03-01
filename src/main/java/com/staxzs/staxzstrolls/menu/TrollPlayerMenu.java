98acb5d9f279aa16f6749428f59b8ec3df6ac48d
Matěj Vnouček <matej.vnoucek@gmail.com>
2024-03-01 19:10:17 +0100

    move items in TrollPlayerMenu

--- src/main/java/com/staxzs/staxzstrolls/menu/TrollPlayerMenu.java
+++ src/main/java/com/staxzs/staxzstrolls/menu/TrollPlayerMenu.java
@@ -1,24 +1,62 @@
 package com.staxzs.staxzstrolls.menu;
 
 import com.staxzs.staxzstrolls.troll.Troll;
+import com.staxzs.staxzstrolls.util.Interval;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.mineacademy.fo.menu.Menu;
 import org.mineacademy.fo.menu.MenuPagged;
+import org.mineacademy.fo.menu.button.Button;
+import org.mineacademy.fo.menu.button.annotation.Position;
 import org.mineacademy.fo.menu.model.ItemCreator;
 import org.mineacademy.fo.remain.CompMaterial;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class TrollPlayerMenu extends MenuPagged<Troll> {
 
 	private final Player target;
 
-	public TrollPlayerMenu(Menu parentMenu, Player viewer, Player target) {
-		super(9 * 4, parentMenu, Troll.getRegisteredTrolls());
+	// Buttons
+	@Position(4)
+	private final Button targetInfo;
 
+	public TrollPlayerMenu(Menu parentMenu, Player viewer, @NotNull Player target) {
+
+		// Initialise menu
+		super(parentMenu, Interval.of(18, 44).toList(), Troll.getRegisteredTrolls());
+		this.setSize(54);
 		this.setViewer(viewer);
 		this.target = target;
 		this.setTitle("&8Trolling &l" + target.getName());
+
+		this.setSlotNumbersVisible();
+
+		// Initialise buttons
+		this.targetInfo = Button.makeDummy(ItemCreator.of(CompMaterial.PLAYER_HEAD)
+				.name("&8Target Info")
+				.skullOwner(target.getName()));
+	}
+
+	@Override
+	public ItemStack getItemAt(int slot) {
+		// Divider
+		List<Integer> dividerIndexes = Arrays.asList(9, 10, 11, 12, 13, 14, 15, 16, 17);
+		if (dividerIndexes.contains(slot))
+			return ItemCreator.of(CompMaterial.BLACK_STAINED_GLASS_PANE).name(" ").make();
+
+		return super.getItemAt(slot);
+	}
+
+	@Override
+	protected String[] getInfo() {
+		return new String[]{
+				"Select a troll to use on",
+				"the player " + this.target.getName() + "."
+		};
 	}
 
 	@Override
